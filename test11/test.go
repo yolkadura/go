55eac1c2f5ae64f7c788e44588649bfbfe48c0f5
yolkadura <37827863+yolkadura@users.noreply.github.com>
2019-06-06 09:56:59 +0300

    Create test.go

---
+++ test11/test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+
+	var (
+		file   string
+		temp   []byte
+		result [][]byte
+		k      int
+		j      int
+	)
+	
+	fmt.Print("What file you want encode?: ")
+	fmt.Scanf("%s", &file) //чтение ввода имени файла юзером
+
+	bs, err := ioutil.ReadFile(file) //bs принимает байты из файла, когда file хранит только имя
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	j = len(bs) //количество символов без знаков
+	//p := make([]int,255) //массив для количества
+	m := make(map[string]int) //мапа
+	for i := 0; i < len(bs); i++ {
+		switch bs[i] {
+		case 32, 46, 44, 33, 63, 92, 10, 13:
+			j = j - 1
+			if temp != nil {
+				result = append(result, temp)
+			}
+			temp = nil
+			continue
+		case 122:
+			k = k + 1
+		}
+		temp = append(temp, bs[i])
+	}
+
+	for i := 0; i < len(bs); i++ {
+		m[string(bs[i])] = strings.Count(string(bs), string(bs[i]))
+	}
+
+	fmt.Println(string(bs))
+	fmt.Println(bs)
+	fmt.Println(result)
+	//fmt.Println(p)
+	fmt.Println(m)
+
+}
