68787b0a59946254375cf7093f718d32a68374b2
yolkadura <37827863+yolkadura@users.noreply.github.com>
2019-06-10 16:12:25 +0300

    Update test2.go

--- ships/test2.go
+++ ships/test2.go
@@ -10,14 +10,18 @@ func main() {
 
 	var (
 		deck [5][5] string //дека
-		p int			//поинт - центр корабля
-		a int			//поинт - рандом для оси
+		p int			//поинт - рандом для оси
+		a int			//строка
+		b int			//столбец
 	)
 	rand.Seed(time.Now().UnixNano())
-	//p = rand.Intn(5)
-	//a = rand.Intn(3)
-	p = 0
-	a = 0
+	p = rand.Intn(2)
+	a = rand.Intn(5)
+	b = rand.Intn(5)
+	
+	// p = 0 //0 - горизонталь, 1 - вертикаль
+	// a = 4 
+	// b = 1
 	
 
 	for i := 0; i < 5; i++ { //заполнение волнами
@@ -27,45 +31,32 @@ func main() {
 	}
 	
 	switch p {
-	case 0:
-		switch a {
-		case 0:
-			deck[0][1] = "o"
-			deck[0][0] = "o"
-			deck[1][0] = "o"
-		case 1:
-			for i := p; i < p+3; i++ {
-				deck[p][i] = "o"
+	case 0: //горизонталь
+			if b != 0 {
+				for i := b-1; i< b+2; i++ {
+					deck[a][i] = "x"
+				} 
+			} else {
+				for i := b; i< b+3; i++ {
+						deck[a][i] = "x"
+				}
 			}
-		case 2:
-			deck[4][0] = "o"
-			deck[3][0] = "o"
-			deck[4][1] = "o"
-		}
-	case 1, 2, 3:
-		switch a {
-		case 0:
-			for i := p-1; i < p+2; i++ {
-				deck[i][p] = "o"
+			
+	case 1: //вертикаль
+			if a != 0 {
+				for i := a-1; i< a+2; i++ {
+					deck[i][b] = "x"
+				} 
+			} else {
+				for i := a; i< a+3; i++ {
+						deck[i][b] = "x"
+				}
 			}
-		case 1:
-			for i := p-1; i < p+2; i++ {
-				deck[p][i] = "o"
-			}
-		}
-		
-	case 4:
-		switch a {
-		case 0:
-			deck[0][4] = "o"
-			deck[0][3] = "o"
-			deck[1][4] = "o"
-		case 1:
-			for i := p-1; i < p+2; i++ {
-				deck[p][i] = "o"
-			}
-		}
 	}
+
+
+
+	//deck[2][3] = "x"
 
 	for j := 0; j < 5; j++ { //вывод
 		fmt.Println(deck[j])
@@ -73,4 +64,5 @@ func main() {
 
 	fmt.Println(p)
 	fmt.Println(a)
+	fmt.Println(b)
 }
