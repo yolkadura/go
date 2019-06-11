d0484288e9c3cef35e64ef2444b163a90269a25c
yolkadura <37827863+yolkadura@users.noreply.github.com>
2019-06-11 13:48:42 +0300

    Update test2.go

--- ships/test2.go
+++ ships/test2.go
@@ -12,7 +12,7 @@ const (
 	n = 5 //длина массива
 )
 
-func ship2 (deck[n][n] string, p, a2, b2 int) {
+func ship2 (deck*[n][n] string, p, a2, b2 int) {
 	switch p { //кораблик 2
 			case 0: //горизонталь
 					if b2 == 0 {
@@ -38,35 +38,7 @@ func ship2 (deck[n][n] string, p, a2, b2 int) {
 		}
 }
 
-func main() {
-
-	var (
-		deck [n][n] string 	//дека
-		p int				//поинт - рандом для оси
-		a int				//строка
-		b int				//столбец
-		a2 int				//строка
-		b2 int				//столбец
-	)
-
-	rand.Seed(time.Now().UnixNano())
-
-	p = rand.Intn(2)
-	a = rand.Intn(n)
-	b = rand.Intn(n)
-	a2 = rand.Intn(n)
-	b2 = rand.Intn(n)
-	
-	// p = 0 //0 - горизонталь, 1 - вертикаль
-	// a = 3 
-	// b = 4
-	
-	for i := 0; i < n; i++ { //заполнение волнами
-		for j := 0; j < n; j++ {
-			deck[i][j] = sea
-		}
-	}
-	
+func ship3 (deck*[n][n] string, p, a, b int) {
 	switch p { //кораблик 3
 	case 0: //горизонталь
 			if b == 0 {
@@ -98,7 +70,35 @@ func main() {
 				} 
 			}
 	}
+}
 
+func main() {
+
+	var (
+		deck [n][n] string 	//дека
+		p int				//поинт - рандом для оси
+		a int				//строка
+		b int				//столбец
+		a2 int				//строка
+		b2 int				//столбец
+	)
+
+	rand.Seed(time.Now().UnixNano())
+
+	p = rand.Intn(2)
+	a = rand.Intn(n)
+	b = rand.Intn(n)
+	a2 = rand.Intn(n)
+	b2 = rand.Intn(n)
+	
+	for i := 0; i < n; i++ { //заполнение волнами
+		for j := 0; j < n; j++ {
+			deck[i][j] = sea
+		}
+	}
+	
+	ship3 (&deck,p,a,b)
+	fmt.Println(p)
 	p = rand.Intn(2)
 	
 	for a2 == a || b2 == b {
@@ -106,36 +106,13 @@ func main() {
 	b2 = rand.Intn(n)
 	}
 	
-	ship2 (deck,p,a2,b2)
-	// switch p { //кораблик 2
-	// 	case 0: //горизонталь
-	// 			if b2 == 0 {
-	// 				for i := b2; i< b2+2; i++ {
-	// 					deck[a2][i] = ship
-	// 				}
-	// 			} else {
-	// 				for i := b2-1; i< b2+1; i++ {
-	// 					deck[a2][i] = ship
-	// 				}
-	// 			}
-				
-	// 	case 1: //вертикаль
-	// 			if a2 == 0 {
-	// 				for i := a2; i< a2+2; i++ {
-	// 					deck[i][b2] = ship
-	// 				}
-	// 			} else {
-	// 				for i := a2-1; i< a2+1; i++ {
-	// 					deck[i][b2] = ship
-	// 				}
-	// 			}
-	// }
-
+	ship2 (&deck,p,a2,b2)
+	
 	for j := 0; j < n; j++ { //вывод
 		fmt.Println(deck[j])
 	}
 
-	fmt.Println(p)
+	
 	fmt.Println("a",a)
 	fmt.Println("b",b)
 	fmt.Println(p)	
