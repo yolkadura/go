40acefc37f128e07bcc1f95545e112aa44a6f79f
yolkadura <37827863+yolkadura@users.noreply.github.com>
2019-08-26 13:19:15 +0300

    Update test3.go

--- ships/test3.go
+++ ships/test3.go
@@ -11,19 +11,20 @@ import (
 const (
 	ship = "o"
 	sea = "~"
-	text string
+
 	//shot = "x"
 	n = 5 //длина массива
 )
 
-func coord (text string) {
+func coord (text string) int {
+	input := bufio.NewScanner(os.Stdin)
 	fmt.Print(text)									
-	_, line, err = input.Scan(), input.Text(), input.Err()		
+	_, line, err := input.Scan(), input.Text(), input.Err()		
 	if err != nil {
 		fmt.Errorf("can't find point, %v", err)
 	}
 
-	x, err = strconv.Atoi(line)
+	x, err := strconv.Atoi(line)
 	if err != nil {
 		fmt.Errorf("invalid point, %v", err)
 	}
@@ -36,12 +37,13 @@ func main() {
 		deck [n][n] string 	//дека
 		x int
 		y int
-		line string
-		err error
+		// line string
+		// err error
+		// text string
 
 	)
 	
-	input := bufio.NewScanner(os.Stdin)
+
 
 	for i := 0; i < n; i++ { //заполнение волнами
 		for j := 0; j < n; j++ {
@@ -55,18 +57,7 @@ func main() {
 
 
 	coord("Куда поставим кораблик? Координата Х: ")
-
-	fmt.Print("Куда поставим кораблик? Координата Y: ")									//новый скан какой-то
-	_, line, err = input.Scan(), input.Text(), input.Err()		//month
-	if err != nil {
-		fmt.Errorf("can't find point, %v", err)
-	}
-
-	y, err = strconv.Atoi(line)
-	if err != nil {
-		fmt.Errorf("invalid point, %v", err)
-	}
-
+	coord("Куда поставим кораблик? Координата Y: ")
 
 	fmt.Print(x,y)
 }
