ab1d42cf00fd518a9872a7bea5bd5fa4bd4692d8
yolkadura <ashley88moshe@gmail.com>
2020-01-21 15:34:14 +0300

    bot

---
+++ bot/main.go
@@ -0,0 +1,20 @@
+package main
+
+import {
+
+}
+
+//точка входа
+func main() {
+
+}
+
+//запросы обновлений
+func getUpdates() {
+
+}
+
+//ответ на обновления
+func respond() {
+
+}
---
+++ bot/models.go
@@ -0,0 +1,16 @@
+package main
+
+
+type Update struct {
+	Update_id int			`json:"update_id"`
+	Message Message			`json:"message"`
+}
+
+type Message struct {
+	Chat Chat				`json:"chat"`
+	Text string				`json:"text"`
+}
+
+type Chat struct {
+	ChatId int				`json:"chatid"`
+}
