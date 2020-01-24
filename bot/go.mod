1eb7073e973463a5190a9e4fc41a1d2be598c2f1
yolkadura <ashley88moshe@gmail.com>
2020-01-24 15:30:59 +0300

    alive

---
+++ bot/go.mod
@@ -0,0 +1,7 @@
+module bot
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
+)
--- bot/main.go
+++ bot/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"golang.org/x/net/proxy"
 )
 
+const PROXY_ADDR = "213.183.59.195:1080"
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("753203683:AAF7UUwaVSihMMa_R2qci-QZqkRgQ3tV1HE")
+
+	//логпассы сокса
+	auth := proxy.Auth{
+		User:     "banya",
+		Password: "supersiski",
+	}
+
+	//настройка подключения
+	dialer, err := proxy.SOCKS5("tcp", PROXY_ADDR, &auth, proxy.Direct)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "can't connect to the proxy:", err)
+		os.Exit(1)
+	}
+
+	// настройки клиента
+	httpTransport := &http.Transport{}
+	httpTransport.Dial = dialer.Dial
+	httpClient := &http.Client{Transport: httpTransport}
+
+	//это была строка без сокса bot, err := tgbotapi.NewBotAPI("753203683:AAF7UUwaVSihMMa_R2qci-QZqkRgQ3tV1HE")
+	bot, err := tgbotapi.NewBotAPIWithClient("753203683:AAF7UUwaVSihMMa_R2qci-QZqkRgQ3tV1HE", httpClient)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -22,15 +48,37 @@ func main() {
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
+		if update.Message == nil { // игнорируем пустые сообщения
 			continue
 		}
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		//эхо
+		// msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		// msg.ReplyToMessageID = update.Message.MessageID
+		// bot.Send(msg)
 
-		bot.Send(msg)
+		if update.Message.Text == "слыш" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "слышу, слышу")
+			bot.Send(msg)
+
+		}
+
+		//тут типа новый мембер, но хз работает ли
+		var reply string
+
+		if update.Message.NewChatMembers != nil {
+			// В чат вошел новый пользователь
+			// Поприветствуем его
+			reply = fmt.Sprintf("Даров @%s! Семки есть?", update.Message.NewChatMembers)
+		}
+		if reply != "" {
+			// Созадаем сообщение
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+			// и отправляем его
+			bot.Send(msg)
+		}
+
 	}
 }
